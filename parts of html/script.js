bd0f5c08cbf09608d61e15c9db13d49f98a68936
AndrewJose18 <86732165+AndrewJose18@users.noreply.github.com>
2021-07-11 14:49:08 +0530

    Add files via upload

---
+++ parts of html/script.js
@@ -0,0 +1,77 @@
+function myFunction() {
+    var selectedDate=document.getElementById("demo").value;
+    console.log(selectedDate);
+    const Http = new XMLHttpRequest();
+    const url='https://noise-pol-montoring-system-default-rtdb.firebaseio.com/Noise(db)_data.json';
+    
+
+    Http.onreadystatechange = () => {
+        if (Http.readyState == 4 && Http.status == 200) {
+            console.log(Http.responseText);
+            var resData=JSON.parse(Http.responseText);
+            console.log(resData);
+            var i;
+            var plotData=[];
+            for(i=0;i<resData.length;i++){
+                var y=resData[i]['reading']
+                if(resData[i]['date']==selectedDate){
+                var temp1=resData[i]['date'].split('-');
+                var temp2=resData[i]['time'].split(':');
+                var x=new Date(temp1[0],temp1[1]-1,temp1[2],temp2[0],temp2[1]);
+                console.log(x,y);
+                plotData.push({x:x,y:parseFloat(y)});
+
+                }
+                
+            }
+            
+            //console.log(plotData);
+            if(plotData.length!=0){
+                var chart = new CanvasJS.Chart("chartContainer",
+                {
+                    title:{
+                        text: "Noise Sensor Data"
+                    },
+                    axisX:{
+                        gridColor: "lightgreen" ,
+                        gridThickness: 2        
+                    },
+                    axisY:{        
+                        gridColor: "lightgreen"
+                    },
+                    data: [
+                        {
+                            type: "line",
+
+                            dataPoints: plotData
+                        }
+
+                    ]
+                });
+
+                chart.render();
+            }
+            else{
+                document.getElementById("chartContainer").innerHTML="No data available for this date."
+            }
+        } 
+    }
+    Http.open("GET", url);
+    Http.send();
+    
+}
+
+/*
+dataSet2=[
+    { x: new Date(2012, 01, 1), y: 44 },
+    { x: new Date(2012, 02, 1), y: 50 },
+    { x: new Date(2012, 03, 1), y: 40 },
+    { x: new Date(2012, 04, 1), y: 40 },
+    { x: new Date(2012, 05, 1), y: 50 },
+    { x: new Date(2012, 06, 1), y: 40 },
+    { x: new Date(2012, 07, 1), y: 40 },
+    { x: new Date(2012, 08, 1), y: 40 },
+    { x: new Date(2012, 09, 1), y: 50 },
+    { x: new Date(2012, 10, 1), y: 40 },
+    { x: new Date(2012, 11, 1), y: 50 }
+];*/
